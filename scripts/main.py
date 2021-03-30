3cb6d52cc26db5d1e7911ed1dbbb05789188b662
Rhonel Tamargo <rhonel.tamargo@sjsu.edu>
2021-03-29 23:51:22 -0700

    Add first practice script and hello.py

---
+++ scripts/main.py
@@ -0,0 +1,12 @@
+print(2 + 2)
+print(5 - 2)
+print(2 * 3)
+print(2 / 2)
+print((2 + 3) * 6)
+print('Hello World!')
+print('Alice' + 'Bob')  # string concatenation
+print('Hello' * 10)  # string replication
+print('Hello' + '!' * 10)  # string concatenation and replication
+
+spam = 42
+print(spam)  
